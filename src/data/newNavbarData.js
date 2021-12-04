e57d411c7e07841da9ec76f52487842727e8848a
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-04 16:15:27 +0500

    Add files via upload

---
+++ src/data/newNavbarData.js
@@ -0,0 +1,22 @@
+export const data = [
+	{
+		to: '/about',
+		text: 'About',
+		id: 'about',
+	},
+	
+	{
+		to: '/donate',
+		text: 'Categories',
+	},
+	{
+		to: '/sponsor',
+		text: 'Sponsor',
+	},
+	{
+		to: '/contact',
+		text: 'Contact',
+		id: 'contact',
+	},
+	
+];
