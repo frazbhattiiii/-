58888891ad82ae1b479285dbb40c69a86f7a27d0
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-07 16:15:34 +0500

    Add files via upload

---
+++ src/pages/DonationForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DonationForm from '../components/donationForm/donationForm'
+function DonationFormPage() {
+  return (
+    <div>
+      <DonationForm />
+    </div>
+  )
+}
+
+export default DonationFormPage
---
+++ src/pages/PricingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Pricing from '../components/Pricing/Pricing';
+
+const PricingPage = () => {
+	return (
+	<>
+	<Navbar></Navbar>
+	<Pricing></Pricing>
+
+	</>
+	);
+};
+
+export default PricingPage;
