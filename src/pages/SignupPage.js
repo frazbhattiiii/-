02b4dc3d5eca877b980b9c9ea602294dfe302632
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-07 16:16:43 +0500

    Add files via upload

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../components/Form/Form';
+import Navbar from '../components/Navbar/Navbar';
+
+function SignUp() {
+	return (
+		<>
+		<Navbar></Navbar>
+			<Form />
+		</>
+	);
+}
+
+export default SignUp;
