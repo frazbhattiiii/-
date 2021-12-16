3598dd281694cbd3635edcce3bdecaddc08f520b
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2021-12-16 20:59:07 +0500

    Updated UI

---
+++ Client/src/src/components/Categories/categories.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+
+import { Button, Heading, TextWrapper } from '../../globalStyles';
+import { IconContext } from 'react-icons/lib';
+import {
+	PricingSection,
+	PricingWrapper,
+	PricingContainer,
+	PricingCardInfo,
+	PricingCardPlan,
+	PricingCardCost,
+	PricingCardFeatures,
+	PricingCardText,
+	PricingCardFeature,
+	PricingCard,
+	priceButton,
+  PriceImageWrapper,
+  PriceImage
+} from './categoriesStyle';
+import {categoriesData} from '../../data/categoriesData';
+
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+function AllCategories() {
+	const history = useHistory();
+
+  const routeChange = () =>{ 
+	
+    let path = `/donation`; 
+    history.push(path);
+  }
+	return (
+		<IconContext.Provider value={{ color: '#a9b3c1', size: '1rem' }}>
+			<PricingSection id="pricing">
+				<PricingWrapper>
+					<Heading> All Categories </Heading>
+
+					<TextWrapper
+						mb="1.4rem"
+						weight="600"
+						size="1.1rem"
+						color="white"
+						align="center"
+					>
+						 
+					</TextWrapper>
+					<PricingContainer>
+          
+						{categoriesData.map((card, index) => (
+							<PricingCard key={index}>
+								<PricingCardInfo>
+                <PriceImageWrapper>
+						  	<PriceImage
+								src={card.image}
+						
+					
+						  	/>			
+              </PriceImageWrapper>
+									<PricingCardPlan>{card.title}</PricingCardPlan>
+									<PricingCardCost>{card.price}</PricingCardCost>
+									<PricingCardText>{card.description}</PricingCardText>
+									<PricingCardFeatures>
+										{card.features.map((feature, index) => (
+											<PricingCardFeature key={index}>
+												{feature}
+											</PricingCardFeature>
+										))}
+									</PricingCardFeatures>
+								
+                  			
+									<Button onClick={routeChange}>Donate
+								
+									</Button>
+											
+								
+
+								</PricingCardInfo>
+							</PricingCard>
+						))}
+					</PricingContainer>
+				</PricingWrapper>
+			</PricingSection>
+		</IconContext.Provider>
+	);
+}
+export default AllCategories;
---
+++ Client/src/src/data/NavbarData.js
@@ -0,0 +1,21 @@
+export const data = [
+	{
+		to: '/',
+		text: 'About',
+		id: 'about',
+	},
+	
+	{
+		to: '/pricing',
+		text: 'Categories',
+	},
+	{
+		to: '/register',
+		text: 'Register',
+	
+	},
+	{
+		to: '/signin',
+		text: 'Contact',
+	},
+];
