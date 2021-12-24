3b8c874a40160ead2c5db9f1efe4e9d6a2b192b4
Moez Ahmad <wyellow19@gmail.com>
2021-12-24 19:46:40 +0500

    Sign out button added

--- Client/src/pages/employeePage.js
+++ Client/src/pages/employeePage.js
@@ -1,9 +1,9 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 import Axios from 'axios'
 import EmployeeInterface from '../components/employee/employee'
 function EmployeePage() {
-  const history = useHistory();
+	const history = useHistory();
 
 	const routeChange = (path) => {
 		history.push(path);
@@ -15,21 +15,33 @@ function EmployeePage() {
 				if (response.data[1] == "admin") {
 					routeChange("/admin")
 				}
-				else if(response.data[1] == "") {
+				else if (response.data[1] == "") {
 					routeChange("/admEmp") //Change it when UI is updated
 				}
-				else if(response.data[1] == "user") {
-					routeChange("/newHome") 
+				else if (response.data[1] == "user") {
+					routeChange("/newHome")
 				}
-				
+
 
 			})
-	})  
-  return (
-    <div>
-      <EmployeeInterface/>
-    </div>
-  )
+	})
+	const signOut = (e)=>{
+		Axios.post("http://localhost:3001/api/signOut/currentUser")
+			.then((response) => {
+				console.log(response.data)
+				if (response.data) {
+					routeChange("/admEmp")
+				}
+			})
+	}
+	return (
+		<div>
+			<div className='flex flex-column items-end'>
+				<button onClick={signOut} className='mt4 ph3 pv2 grow br3 bg-dark-blue b-dark-blue b white'>Sign Out</button>
+			</div>
+			<EmployeeInterface />
+		</div>
+	)
 }
 
 export default EmployeePage
